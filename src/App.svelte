df3b79e85ea7c8cdcbf3b08cb4bdd6f938d71f0f
Naejin Nam <salvia1223@naver.com>
2023-04-19 14:17:24 +0900

    #3 Slot 개념 테스트

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
 <script>
 	export let name;
+    import Child from "./Child.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>I'm Parent</h1>
+	
+	<Child>
+		<p slot="name">손흥민</p>
+		<p slot="age">24</p>
+		<p>속성을 정의하지 않은 Slot.</p>
+	</Child>
+	
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/Child.svelte
@@ -0,0 +1,11 @@
+<div>
+    <h1>I'm Child</h1>
+
+    <p><slot name="name">전달받은 이름이 없습니다.</slot></p>
+
+    <slot name="age">전달받은 나이가 없습니다.</slot>
+
+    <slot name="phone">전달받은 전화번호가 없습니다.</slot>
+    
+    <slot>전달받은 콘텐츠가 없습니다.</slot>
+</div>
